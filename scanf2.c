f03435eb74e13dcf90d039d93cffb479c278a3fa
yuta-126 <t236021@st.yamagata-u.ac.jp>
2024-10-18 19:26:48 +0900

    Add files via upload

---
+++ scanf2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("How many dogs?");
+	scanf("%d",&a);
+	printf("How many cows?");
+	scanf("%d",&b);
+	c=a+b;
+	printf("animals is %d",c);
+
+	return 0;
+}
