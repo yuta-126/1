79b8d2d195aabd1ef577bbbc9ba6220006240d09
yuta-126 <t236021@st.yamagata-u.ac.jp>
2024-11-01 18:47:46 +0900

    Add files via upload

---
+++ float.h
@@ -0,0 +1,14 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("double型の最大値:%g\n", DBL_MAX);
+	printf("double型の最小値:%g\n", DBL_min);
+	printf("double型の制度:%d\n", DBL_DIG);
+	printf("int型の最大値:%d\n", INT_MAX);
+	printf("int型の最小値:%d\n", INT_MIN);
+	
+	return θ;
+}
