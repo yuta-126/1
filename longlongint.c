c5af5cd8715a6a64040fb20d19c37362689de2dc
yuta-126 <t236021@st.yamagata-u.ac.jp>
2024-11-01 19:27:31 +0900

    Add files via upload

---
+++ longlongint.c
@@ -0,0 +1,11 @@
+int main(void)
+{
+	printf("double型の最大値:%g\n", DBL_MAX);
+	printf("double型の最小値:%g\n", DBL_MIN);
+	printf("double型の制度:%d\n", DBL_DIG);
+	printf("long long int型の最大値:%lld\n", LLONG_MAX);
+	printf("long long int型の最小値:%lld\n", LLONG_MIN);
+	printf("long long int型の符号なし最大値:%lld\n", ULLONG_MAX);
+	
+	return 0;
+}
